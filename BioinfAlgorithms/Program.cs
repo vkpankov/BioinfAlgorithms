0f33e07655a501e8a76df1f42c71682a78e188ef
vkpankov <vkpankov@gmail.com>
2019-10-20 00:00:20 +0300

    tt

--- BioinfAlgorithms/Program.cs
+++ BioinfAlgorithms/Program.cs
@@ -34,10 +34,25 @@ namespace BioinfAlgorithms
             string affGapTS2 = "AATTGCCGCCGTCGTTTTCAGCAGTTATGTCAGATC";//"AATTGCCGCCGTCGTTTTCAGCAGTTATGTCAGATC";
 
             var ag = new AffinityGapsAlignment();
-            Console.WriteLine("Тест 1 (gap=-1, mismatch=-1):");
-            ag.MismatchPenalty = -5;
+            Console.WriteLine("Тест 1 (gap=-1, mismatch=-1, open = 0):");
+            ag.MismatchPenalty = -1;
             ag.GapPenalty = -1;
+            ag.GapOpenPenalty = 0;
             Console.WriteLine(ag.CalcAlignment(affGapTS1, affGapTS2));
+
+            Console.WriteLine("Тест 2 (gap=-1, mismatch=-1):");
+            ag.MismatchPenalty = -1;
+            ag.GapPenalty = -0.01;
+            ag.GapOpenPenalty = -100;
+            Console.WriteLine(ag.CalcAlignment(affGapTS1, affGapTS2));
+
+
+            Console.WriteLine("Тест 3 (gap=-1, mismatch=-1):");
+            ag.MismatchPenalty = -1;
+            ag.GapPenalty = -0.3;
+            ag.GapOpenPenalty = 0.5;
+            Console.WriteLine(ag.CalcAlignment(affGapTS1, affGapTS2));
+
 
             Console.ReadKey();
         }
