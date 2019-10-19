0e00b543d770ed9127d115edf90e624366b5857f
vkpankov <vkpankov@gmail.com>
2019-10-19 23:57:07 +0300

    Affinity gaps

---
+++ BioinfAlgorithms/AffinityGapsAlignment.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BioinfAlgorithms
+{
+    public class AffinityGapsAlignment
+    {
+
+        enum StepType { M, Gaps1, Gaps2}
+
+        public double[,] SimilarityMatrix { get; set; }
+
+        //Фиксированные штрафы; используются, если не задана матрица весов
+        public double MismatchPenalty { get; set; }
+        public double GapOpenPenalty { get; set; }
+        public double GapPenalty { get; set; } 
+        public Dictionary<char, int> LetterIndexPairs { get; set; }
+
+        private AlignmentResult GetAlignmentByAncestorsMatrix(string S1, string S2, StepType[,] pointersM, StepType[,] pointersGaps1,
+            StepType[,] pointersGaps2, StepType startStep)
+        {
+            int k = S1.Length - 1;
+            int l = S2.Length - 1;
+
+            AlignmentResult result = new AlignmentResult();
+
+            StepType curStep = startStep;
+            pointersM[S1.Length - 1, S2.Length - 1] = StepType.M;
+            while (k > 0 && l > 0)
+            {
+                if (curStep == StepType.M && k>0&&l>0)
+                {
+                    result.S1 = S1[k] + result.S1;
+                    result.S2 = S2[l] + result.S2;
+                    k--;
+                    l--;
+                    curStep = pointersM[k, l];
+                }
+                else
+                if(k>0 && curStep == StepType.Gaps1)
+                {
+                    result.S1 = S1[k] + result.S1;
+                    result.S2 = "-" + result.S2;
+                    k--;
+                    curStep = pointersGaps1[k, l];
+                }
+                else if(l>0)
+                {
+                    result.S1 = "-" + result.S1;
+                    result.S2 = S2[l] + result.S2;
+                    l--;
+                    curStep = pointersGaps2[k, l];
+                }
+            }
+            return result;
+            
+        }
+
+        public AlignmentResult CalcAlignment(string S1, string S2)
+        {
+            double[,] M = new double[S1.Length, S2.Length];
+            double[,] Gaps1 = new double[S1.Length, S2.Length];
+            double[,] Gaps2 = new double[S1.Length, S2.Length];
+
+            StepType[,] MPointers = new StepType[S1.Length, S2.Length]; //0
+            StepType[,] Gaps1Pointers = new StepType[S1.Length, S2.Length]; //1
+            StepType[,] Gaps2Pointers = new StepType[S1.Length, S2.Length]; //2
+
+
+            for (int i = 1; i < S1.Length; i++)
+                M[i, 0] = double.NegativeInfinity;
+            for (int j = 1; j < S2.Length; j++)
+                M[0, j] = double.NegativeInfinity;
+
+
+            for (int i = 0; i < S1.Length; i++)
+            {
+                Gaps1[i, 0] = GapOpenPenalty + i * GapPenalty;
+                Gaps2[i, 0] = double.NegativeInfinity;
+            }
+
+            for (int i = 0; i < S2.Length; i++)
+            {
+                Gaps1[0, i] = double.NegativeInfinity;
+                Gaps2[0, i] = GapOpenPenalty + i * GapPenalty;
+
+            }
+
+            for (int i = 1; i < S1.Length; i++)
+            {
+                for (int j = 1; j < S2.Length; j++)
+                {
+
+                    double score1 = 0;
+                    double score2 = 0;
+                    double score3 = 0;
+
+                    double pairMatch;
+                    if (SimilarityMatrix != null)
+                    {
+                        pairMatch = SimilarityMatrix[LetterIndexPairs[S1[i]], LetterIndexPairs[S2[j]]];
+                    }
+                    else
+                    {
+                        pairMatch = (S1[i] == S2[j] ? 1 : MismatchPenalty);
+                    }
+                    score1 = M[i - 1, j - 1] + pairMatch;
+                    score2 = Gaps1[i - 1, j - 1] + pairMatch;
+                    score3 = Gaps2[i - 1, j - 1] + pairMatch;
+
+                    if (score1 >= score2 && score1 >= score3)
+                    {
+                        M[i, j] = score1;
+                        MPointers[i, j] = StepType.M;
+                        //Pointer matrix for M to M
+                    }
+                    else if (score2 >= score1 && score2 >= score3)
+                    {
+                        M[i, j] = score2;
+                        MPointers[i, j] = StepType.Gaps1;
+                        //Pointer matrix for M to Gaps1
+                    }
+                    else
+                    {
+                        M[i, j] = score3;
+                        MPointers[i, j] = StepType.Gaps2;
+                        //Pointer matrix for M to Gaps2
+                    }
+
+                    score1 = GapOpenPenalty + GapPenalty + M[i, j - 1];
+                    score2 = GapPenalty + Gaps1[i, j - 1];
+                    score3 = GapOpenPenalty + GapPenalty + Gaps2[i, j - 1];
+
+
+                    if (score1 >= score2)
+                    {
+                        Gaps1[i, j] = score1;
+                        Gaps1Pointers[i, j] = StepType.M;
+                        //Pointer matrix for Gaps1 to M
+                    }
+                    else if (score2 >= score1 && score2 >= score3)
+                    {
+                        Gaps1[i, j] = score2;
+                        Gaps1Pointers[i, j] = StepType.Gaps1;
+                        //Pointer matrix for Gaps1 to Gaps1
+                    }
+                    else
+                    {
+                        Gaps1[i, j] = score3;
+                        Gaps1Pointers[i, j] = StepType.Gaps2;
+                        //Pointer matrix for Gaps1 to Gaps2
+                    }
+
+                    score1 = GapOpenPenalty + GapPenalty + M[i - 1, j];
+                    score2 = GapPenalty + Gaps2[i - 1, j];
+                    score3 = GapOpenPenalty + GapPenalty + Gaps1[i - 1, j];
+
+                    if (score1 >= score2 && score1 >= score3)
+                    {
+                        Gaps2[i, j] = score1;
+                        Gaps2Pointers[i, j] = StepType.M;
+                        //Pointer matrix for Gaps2 to M
+                    }
+                    else if (score2 >= score1 && score2 >= score3)
+                    {
+                        Gaps2[i, j] = score2;
+                        Gaps2Pointers[i, j] = StepType.Gaps2;
+                        //Pointer matrix for Gaps2 to Gaps2
+                    }
+                    else
+                    {
+                        Gaps2[i, j] = score3;
+                        Gaps2Pointers[i, j] = StepType.Gaps1;
+                        //Pointer matrix for Gaps2 to Gaps1
+                    }
+
+
+
+                }
+            }
+
+            double s1 = Gaps1[S1.Length - 1, S2.Length - 1];
+            double s2 = Gaps2[S1.Length - 1, S2.Length - 1];
+            double s3 = M[S1.Length - 1, S2.Length - 1];
+            double maxScore = 0;
+
+            var stepType = StepType.M;
+            if (s1 >= s2 && s1 >= s3)
+            {
+                maxScore = s1;
+                stepType = StepType.Gaps1;
+
+            }
+            else if (s2 >= s1 && s2 >= s3)
+            {
+                maxScore = s2;
+                stepType = StepType.Gaps2;
+            }
+            else
+            {
+                maxScore = s3;
+                stepType = StepType.M;
+            }
+
+            var alignmentResult = GetAlignmentByAncestorsMatrix(S1, S2, MPointers, Gaps1Pointers, Gaps2Pointers, stepType);
+
+ 
+            alignmentResult.Score = maxScore;
+            return alignmentResult;
+        }
+
+
+    }
+}
--- BioinfAlgorithms/Program.cs
+++ BioinfAlgorithms/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,70 +9,35 @@ namespace BioinfAlgorithms
 {
     class Program
     {
+        static string SGen(int length, Random rnd)
+        {
+            string s = "";
+            for (int i = 0; i < length; i++)
+            {
+                double rndDouble = rnd.NextDouble();
+                if (rndDouble < 0.25)
+                    s += "A";
+                else if (rndDouble < 0.50)
+                    s += "C";
+                else if (rndDouble < 0.75)
+                    s += "G";
+                else if (rndDouble < 1)
+                    s += "T";
+
+            }
+            return s;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Глобальное выравнивание без матрицы весов:");
-            string testS1 = "CCCACCGCGTAGTGTAGAGACTAGAT", testS2 = "GCGCGATAGCTAGCTTAGCGCTAAGT";
-            var ndm = new NeedlemanWunschAlgorithm();
+
+            string affGapTS1 = "TCCCAGTTATGTCAGGGGACACGAGCATGCAGAGAC";//"TCCCAGTTATGTCAGGGGACACGAGCATGCAGAGAC";
+            string affGapTS2 = "AATTGCCGCCGTCGTTTTCAGCAGTTATGTCAGATC";//"AATTGCCGCCGTCGTTTTCAGCAGTTATGTCAGATC";
+
+            var ag = new AffinityGapsAlignment();
             Console.WriteLine("Тест 1 (gap=-1, mismatch=-1):");
-            ndm.MismatchPenalty = -1;
-            ndm.GapPenalty = -1;
-            var alignment = ndm.CalcAlignment(testS1, testS2);
-            Console.WriteLine(alignment);
-
-            Console.WriteLine("");
-            Console.WriteLine("Тест 2 (gap=-0.499, mismatch=-1):");
-            ndm.MismatchPenalty = -1;
-            ndm.GapPenalty = -0.499;
-            alignment = ndm.CalcAlignment(testS1, testS2);
-            Console.WriteLine(alignment);
-
-            Console.WriteLine("");
-            Console.WriteLine("Дополнительный тест (gap=-0.499, mismatch=-1):");
-            ndm.MismatchPenalty = -1;
-            ndm.GapPenalty = -0.499;
-            alignment = ndm.CalcAlignment("CGAGAGGAGCGATATTTAGAGTGTGTAGCGAGAGCTCGATCGCTAGCTAGG", "GGGGATAGCTAGTTCGAGATCGATCGAGATCTT");
-            Console.WriteLine(alignment);
-
-            Console.WriteLine("");
-            Console.WriteLine("Глобальное выравнивание с матрицей весов:");
-            ndm.SimilarityMatrix = new double[,] {
-                     {10,-1,-3,-4 },
-                     {-1,7,-5,-3 },
-                     {-3,-5,9,0 },
-                     {-4,-3,0,8 } };
-            ndm.GapPenalty = -1;
-            ndm.LetterIndexPairs = new Dictionary<char, int>() {
-             {'A',0 },{'C',1},{'G',2},{'T',3} };
-            alignment = ndm.CalcAlignment(testS1, testS2);
-            Console.WriteLine(alignment);
-
-            Console.WriteLine("");
-            Console.WriteLine("Заменили вес совпадения буквы С с 10 на 100:");
-            ndm.SimilarityMatrix[1, 1] = 100;
-            alignment = ndm.CalcAlignment(testS1, testS2);
-            Console.WriteLine(alignment);
-            ndm.SimilarityMatrix[1, 1] = 10;
-
-            Console.WriteLine("");
-            Console.WriteLine("Локальное выравнивание:");
-            string localTestS1 = "AATTGCCGCCGTCGTTTTCACAGTTATGTCAGGATC";
-            string localTestS2 = "TCCAGTTATGTCAGGGGGACACGAGCATGCAGAGAC";
-
-            var smw = new SmithWatermanAlgorithm();
-            //smw.SimilarityMatrix = ndm.SimilarityMatrix;
-            //smw.LetterIndexPairs = ndm.LetterIndexPairs;
-            smw.GapPenalty = -1;
-            smw.MismatchPenalty = -1;
-            alignment = smw.CalcAlignment(localTestS1, localTestS2);
-            Console.WriteLine(alignment);
-
-            Console.WriteLine("Тест: глобальное выравнивание для этого же случая:");
-            ndm.GapPenalty = -1;
-            ndm.MismatchPenalty = -1;
-            ndm.SimilarityMatrix = null;
-            alignment = ndm.CalcAlignment(localTestS1, localTestS2);
-            Console.WriteLine(alignment);
+            ag.MismatchPenalty = -5;
+            ag.GapPenalty = -1;
+            Console.WriteLine(ag.CalcAlignment(affGapTS1, affGapTS2));
 
             Console.ReadKey();
         }
